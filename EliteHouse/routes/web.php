8d9cdfa333992b56d7005ef011a791d3f2429ec0
mariam trebak <mariam.trebak@gmail.com>
2025-02-07 13:33:08 +0100

    tercera confirmación

--- EliteHouse/routes/web.php
+++ EliteHouse/routes/web.php
@@ -6,6 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/greeting', function(){
-    return 'Hola mundo';
+Route::get('/equipo', function(){
+    return '<h1> Devolucion de los equipos <h1>';
+});
+
+route::get('/equipo/{id}', function($identificador){
+    return '<h1>Equipo </h1>'.$identificador;
+});
+
+Route::get('/equipo/{id}/comentario/{comment}', function($id,$comentario){
+    return 'Equipo '.$id.', Excelente trabajo '.$comentario;
+});
+
+Route::get('/usuario/{nombre?}', function($nombre = 'Mariam'){
+    return '<h1>Nombre </h1>'.$nombre;
 });
